4c2f57c7958d31c6a2d58a149d10ac2536a38241
LeeRaybone <leeraybone@sky.com>
2026-04-30 19:41:42 +0100

    Normal Items Updater implemented and tests passing

--- GildedRose/Updaters/NormalItemUpdater.cs
+++ GildedRose/Updaters/NormalItemUpdater.cs
@@ -8,6 +8,7 @@ public class NormalItemUpdater : ItemUpdater
 
     public override void Update()
     {
-        throw new NotImplementedException();
+        DecrementSellIn(); // Decrement the sell in days by 1
+        AdjustQuality(Item.SellIn < 0 ? -2 : -1); // Update the quality - if past selll by date decrease by 2, otherwise by 1
     }
 }
